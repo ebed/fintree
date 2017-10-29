c2b3edb57b7e88db0b7ba6991d362130c7dd4f76
Rodrigo Merino <ebedmail@gmail.com>
2017-10-29 20:20:31 -0300

    Ajustes bugs para heroku
    Se ajustan detalles de sintaxis para deploy

--- app/controllers/rest/v1/api_controller.rb
+++ app/controllers/rest/v1/api_controller.rb
@@ -1,7 +1,7 @@
 module Rest
   module V1
     class ApiController <  ApplicationController
-      skip_before_action :verify_authenticity_token  
+     # skip_before_action :verify_authenticity_token  
       before_action :cors_preflight_check
       after_action :cors_set_access_control_headers
 

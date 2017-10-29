e7280bb45aaf33326360dfc255c13353c40cd5a8
Rodrigo Merino <ebedmail@gmail.com>
2017-10-29 20:40:14 -0300

    Se crea el archivo seed con el usuario inicial

--- app/controllers/rest/v1/authentication_controller.rb
+++ app/controllers/rest/v1/authentication_controller.rb
@@ -6,7 +6,7 @@ module Rest
 
 
       def login
-        url_verify = "http://"+ENV['HOSTNAME_PORT']+"/rest/verify_user/#{params[:email]}"
+        url_verify = ENV['PROTOCOLO_SERVER']+"://"+ENV['HOSTNAME_PORT']+"/rest/verify_user/#{params[:email]}"
         response = HTTParty.post(url_verify,{body: { "image": "#{params[:image]}" }})
         
         ahora =Time.now
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create( email: 'ignacio@acid.cl', image: 'TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=')

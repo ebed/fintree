4c0e0487b88b6d5be7f45ead57bd0b3e828c2dcf
Rodrigo Merino <ebedmail@gmail.com>
2017-10-30 18:36:41 -0300

    Ajustes nginx heroku

--- app/controllers/rest/v1/api_controller.rb
+++ app/controllers/rest/v1/api_controller.rb
@@ -1,14 +1,7 @@
 module Rest
   module V1
     class ApiController <  ApplicationController
-      ActiveRecord::RecordNotFound, with: :record_not_found
-
-      ##
-      # Metodo que maneja el caso de posible elmento no existe 
-      def record_not_found
-        render :json =>  {:code => 200, :descripcion => self.class.name+" con el id "+params[:id]+" no existe"}  
-      end
-
+     
    
 
     end

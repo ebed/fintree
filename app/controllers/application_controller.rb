ca4c66160ab9e77212429a5db6b0a04ba2ad8c84
Rodrigo Merino <ebedmail@gmail.com>
2017-10-30 18:41:05 -0300

    Ajustes nginx heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,23 @@
 class ApplicationController < ActionController::Base
   require 'httparty'
   rescue_from Timeout::Error, :with => :rescue_from_timeout
-
+  rescue_from ActiveRecord::RecordNotFound, :with => :rescue_from_notfound
   ##
   # Metodo para recuperar los casos de Timeout, al solo tener
   def rescue_from_timeout(exception)
     flash[:error] = "Problema con el Servicio, intente nuevamente (TIMEOUT)"
     redirect_to root_path
   end
+
+
+  ##
+  # MEtodo para recuperar el caso de buscar registros no existntes
+  
+  def rescue_from_notfound
+  	flash[:error] = "No existe el registro indicado"
+    redirect_to users_path
+  end
+
+
+   
 end

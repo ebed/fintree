03627d221ea9ccff41e71686ba7c6177dd33057d
Rodrigo Merino <ebedmail@gmail.com>
2017-10-30 18:29:39 -0300

    Ajustes nginx heroku

--- app/controllers/rest/v1/api_controller.rb
+++ app/controllers/rest/v1/api_controller.rb
@@ -1,47 +1,16 @@
 module Rest
   module V1
     class ApiController <  ApplicationController
-     # skip_before_action :verify_authenticity_token  
-      before_action :cors_preflight_check
-      after_action :cors_set_access_control_headers
+      ActiveRecord::RecordNotFound, :with => :record_not_found
 
-      #skip_before_action :verify_authenticity_token, only: [:create, :destroy]
-
-      rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
-
-
-
-   #   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
+      ##
+      # Metodo que maneja el caso de Registro no existenet
       def record_not_found
         render :json =>  {:code => 200, :descripcion => self.class.name+" con el id "+params[:id]+" no existe"} # Assuming you have a template named 'record_not_found'
       end
 
+   
 
-
-
-      # For all responses in this controller, return the CORS access control headers.
-
-      def cors_set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DESTROY'
-        headers['Access-Control-Max-Age'] = "1728000"
-      end
-
-      # If this is a preflight OPTIONS request, then short-circuit the
-      # request, return only the necessary headers and return an empty
-      # text/plain.
-
-      def cors_preflight_check
-        if request.method == :options
-          headers['Access-Control-Allow-Origin'] = '*'
-          headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DESTROY'
-          headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-          headers['Access-Control-Max-Age'] = '1728000'
-          render :text => '', :content_type => 'text/plain'
-        end
-      end
-
-     
     end
   end
 end
--- app/controllers/rest/v1/authentication_controller.rb
+++ app/controllers/rest/v1/authentication_controller.rb
@@ -1,4 +1,4 @@
-## 
+##
 # Clase encargada de proveer la API con servicios REST para autenticar
 
 module Rest
@@ -8,64 +8,63 @@ module Rest
 
       ##
       # Servicio de Login expuesto para logear. Se requiere un email y una imagen en BASE64
+      # Se setea en el encabezado un token para usar como llave al servicio 2
       def login
         token = hashconection
         url_verify = ENV['PROTOCOLO_SERVER']+"://"+ENV['HOSTNAME_PORT']+"/rest/verify_user/#{params[:email]}"
-        
         response = HTTParty.post(url_verify, body: { "image": "#{params[:image]}" }, headers: { Authorization: "Bearer #{token}"})
-        
-        ahora =Time.now
-        SendEmailJob.set(wait: 20.seconds).perform_later(params[:email], request.user_agent, ahora.to_s, response.code)  
-
-
+        SendEmailJob.set(wait: 20.seconds).perform_later(params[:email], request.user_agent, Time.now.to_s, response.code)
         render json: { "message": response["message"] }, status: response.code
-
       end
 
-      ## 
+
+      ##
       # Servicio consumido por Login el que valida que la imagen funcione. Se requiere un email  como ID y una imagen en BASE64
+      # Se asegura que el HASH de la IP donde está alojado el servicio sea equivalente al HASH recibido en el header
       def verify
-        header = request.headers[:Authorization]
-        token = header.split[1] unless header.blank?
-       
-        if token != hashconection  
-          render json: {"message": "ACCESO NO PERMITIDO"}, status: 403
-        else
+        if validahash(request.headers[:Authorization])
           usuario = User.where(email: params[:id]+"."+params[:format]).first
           if usuario.present?
-             if ImageCompare.cumpleSemejanza(verify_params[:image], usuario.image)
-             
-              render json: { "message":"OK" }, status: 200
-            else
-              
-              render json: { "message":"No Autorizado"}, status: 401
-            end
+            render json: { "message":"OK" }, status: 200 if ImageCompare.cumpleSemejanza(verify_params[:image], usuario.image)
+            render json: { "message":"No Autorizado"}, status: 401 unless ImageCompare.cumpleSemejanza(verify_params[:image], usuario.image)
           else
-           
             render json: { "message":"No Autorizado"}, status: 401
           end
+        else
+          render json: {"message": "ACCESO NO PERMITIDO"}, status: 403
         end
       end
 
 
       private
       ##
-      # Strong Parameters de imagen 
+      # Strong Parameters de imagen
       def verify_params
         params.permit(:image)
       end
 
-
-       def hashconection
+      ##
+      # Encriptacion de la IP del servidor que servirá como token
+      def hashconection
         md5 = Digest::MD5.new
         md5.update  serverip
         md5.hexdigest
       end
 
-      
 
+      ##
+      # Obtencion de la IP del servidor
       def serverip
-        (Socket.ip_address_list.detect{|intf| intf.ipv4_private? }).ip_address 
+        (Socket.ip_address_list.detect{|intf| intf.ipv4_private? }).ip_address
+      end
+
+
+      ##
+      # Valida token
+
+      def validahash(header)
+        token = header.split[1] unless header.blank?
+        hashconection == token
       end
 
 
